a2771ecf3528999441c340b81b2be65a96ed8989
Rob Thuleen <rob.thuleen@gmail.com>
2018-07-16 16:51:14 -0700

    Add input for board dimension; Update css

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,7 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      board: [
-        ["", "", "", ""],
-        ["", "", "", ""],
-        ["", "", "", ""],
-        ["", "", "", ""]
-      ],
+      board: null,
       words: []
     }
     this.submitBoard = this.submitBoard.bind(this);
@@ -23,6 +18,22 @@ class App extends Component {
     this.setState({ board: board })
   }
 
+  initBoard(ev) {
+    let dimension = ev.target.value;
+    let board = [];
+    for (let i = 0; i < dimension; i++) {
+      let row = [];
+      for (let j = 0; j < dimension; j++) {
+        row.push('');
+      }
+      board.push(row);
+    }
+
+    this.setState({
+      board: board,
+      words: []
+    })
+  }
 
   async submitBoard() {
     let resp = await fetch('/board', {
@@ -41,24 +52,34 @@ class App extends Component {
 
   render() {
     let wordList = this.state.words.map((word, i) => {
-      return <div key={i} className='word'>{word}</div>
+      return <div className = 'word' key={i} className='word'>{word}</div>
     });
 
-    let board = this.state.board.map((col, i) => {
-      let row = col.map((cell, j) => {
-        // return an input with the onChange bound to the current row, col of the board
-        return <input onChange={this.changeCellValue.bind(this, i, j)} type='text' key={j} className='cell' value={cell} />
+    let board = null;
+    if (this.state.board) {
+      board = this.state.board.map((col, i) => {
+        let row = col.map((cell, j) => {
+          // return an input with the onChange bound to the current row, col of the board
+          return <input onChange={this.changeCellValue.bind(this, i, j)} type='text' key={j} className='cell' value={cell} />
+        })
+        return <div key={i} >{row}</div>
       })
-      return <div key={i} >{row}</div>
-    })
+    }
+
 
     return (
 
       <div className="App" >
         <header className="App-header">
           <h1 className="App-title">Welcome to Boggle Solver</h1>
+          <h1 className="App-author">Rob Thuleen</h1>
         </header>
         <div className='body'>
+
+          <div>
+            <span>Enter Board Size: </span>
+            <input className = 'board-size' onChange={this.initBoard.bind(this)} />
+          </div>
           <div className='buttonRow'>
             <button onClick={this.submitBoard}>Solve</button>
           </div>
